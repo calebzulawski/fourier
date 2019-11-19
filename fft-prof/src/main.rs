492256741e6639b1493e8869c7cfb38ea1e23b92
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-11-19 16:33:24 -0500

    Add fft profiler

---
+++ fft-prof/src/main.rs
@@ -0,0 +1,23 @@
+use clap::{App, Arg};
+use num_complex::Complex;
+use rand::{distributions::Standard, Rng};
+
+fn main() {
+    let matches = App::new("fft-prof")
+        .arg(Arg::with_name("size").takes_value(true).required(true))
+        .get_matches();
+
+    let size = usize::from_str_radix(matches.value_of("size").unwrap(), 10).unwrap();
+    let mut fft = fft::Fft32::new(size);
+
+    let mut input = rand::thread_rng()
+        .sample_iter(&Standard)
+        .zip(rand::thread_rng().sample_iter(&Standard))
+        .take(size)
+        .map(|(x, y)| Complex::new(x, y))
+        .collect::<Vec<_>>();
+
+    loop {
+        fft.fft_in_place(&mut input);
+    }
+}
